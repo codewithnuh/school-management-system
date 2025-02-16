3db1d4e6d8cb4dab478d8fb72ce0fafd5b1486fd
nuh25792 <nuh25792@gmail.com>
2025-02-16 10:14:40 +0000

    feat: Introduce Admin model, remove Session model

---
+++ src/models/Admin.ts
@@ -0,0 +1,78 @@
+import { DataTypes } from 'sequelize';
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+} from 'sequelize-typescript';
+import { z } from 'zod';
+
+const adminSchema = z.object({
+    firstName: z.string(),
+    middleName: z.string().optional(),
+    lastName: z.string(),
+    email: z.string().email(),
+    phoneNo: z.string(),
+    password: z.string(),
+    role: z.string(),
+    isSuperAdmin: z.boolean(),
+    createdAt: z.date().optional(),
+    updatedAt: z.date().optional()
+});
+
+type AdminAttributes = z.infer<typeof adminSchema>;
+
+@Table({
+    tableName: 'admins',
+    timestamps: true,
+})
+export class Admin extends Model<AdminAttributes> implements AdminAttributes {
+    @Column({
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    })
+    id!: number;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    firstName!: string;
+
+    @Column({ type: DataType.STRING })
+    middleName?: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    lastName!: string;
+
+    @Column({
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false,
+        validate: { isEmail: true },
+    })
+    email!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+        validate: { isNumeric: true, len: [10, 15] },
+    })
+    phoneNo!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    password!: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    role!: string;
+
+    @Column({ type: DataType.BOOLEAN, defaultValue: false, allowNull: false })
+    isSuperAdmin!: boolean;
+
+    @Column({ type: DataType.DATE, defaultValue: DataTypes.NOW() })
+    createdAt?: Date;
+
+    @Column({ type: DataType.DATE, defaultValue: DataTypes.NOW() })
+    updatedAt?: Date;
+}
